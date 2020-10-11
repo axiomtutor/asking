83fac28ba741b7cefde3c38426aec0bcecf4b223
axiomtutor <axiomtutor@gmail.com>
2020-10-10 23:11:32 -0400

    develop note script

--- mymdTOhtml.py
+++ mymdTOhtml.py
@@ -6,7 +6,7 @@ import re
 Establishing file paths, open files
 '''
 mymdPath = PurePosixPath(Path.cwd() / sys.argv[1])
-mymd = open(mymdPath, "r")
+mymd_file = open(mymdPath, "r")
 
 # Slice the directory where the mymd file is located.
 mymdDir = mymdPath.parent
@@ -15,9 +15,9 @@ name = mymdPath.name
 
 askingPath = "/home/addem/Desktop/asking"
 
-template = open(Path(askingPath + "/html/notes/noteTemplate.html"), "r")
+template_file = open(Path(askingPath + "/html/notes/noteTemplate.html"), "r")
 
-html = open(dir / (name[:-4] + ".html"), "w")
+html_file = open(mymdDir / (name[:-4] + ".html"), "w")
 
 '''
 Write the HTML top matter
@@ -29,36 +29,81 @@ def relativeTOabsolutePATH(line):
     '''
     # for every href
     for match in re.findall('href="(.+?)"', line):
-        print(line[:10])
         # the address it should have is relative to the folder holding the mymd
-        addy = Path.resolve(mymdDir / match[6:-1]))
-        line.replace(match, addy)
+        addy = mymdDir / match[6:]
+        line.replace(match, str(addy))
     return line
 
-# Lines 1 to 11 copy over
+# Lines 1 to 11 of the template copy over
 for i in range(10):
-    line = relativeTOabsolutePATH(template.readline())
-    html.write(line)
+    line = relativeTOabsolutePATH(template_file.readline())
+    html_file.write(line)
 # Line 12 insert title
-mymdLine = mymd.readline()
+mymdLine = mymd_file.readline()
 if not (mymdLine[:8] == 'Title - '):
     print("\nTitle not formatted properly.  It should look like 'Title - ...'")
 htmlLine = '    <title>' + mymdLine[8:] + '</title>'
-html.write(htmlLine)
-template.readline()
-# lines 13 to 63
+html_file.write(htmlLine)
+template_file.readline()
+# lines 13 to 63 copy
 for i in range(51):
-    line = relativeTOabsolutePATH(template.readline())
-    html.write(line)
+    print(i)
+    line = relativeTOabsolutePATH(template_file.readline())
+    html_file.write(line)
 
 '''
 Process the mymd
 '''
-while
+def process_header(line):
+    # Number of stars indicates level of header.
+    if line[0] == "*":
+        htmlLine = "<h"
+        if line[1] == "*":
+            if line[2] == "*":
+                if line[3] == "*":
+                    n = 4
+                else:
+                    n = 3
+            else:
+                n = 2
+        else:
+            n = 1
+        htmlLine += str(n)+">"
+        # Now put everything else in the file, and close header.
+        end_index_of_stars = re.search('*+').span[1]
+        htmlLine += line[end_index_of_stars+1:]+) + "</h" + str(n) + ">"
+    else: # If there are no stars, somehting went wrong.
+        raise("process_header applied to a line that doesn't start with *")
+
+def process_button(section):
+    # A section is a string of all the text inside a button environment.
+    return "process_botton TODO\n"
+
+mymdLine = mymd_file.readline()
+while mymdLine:
+    if (len(mymdLine) == 0 || mymdLine.isspace()):
+        html_file.write("<br>")
+    else:
+        if mymdLine[0] == "*":
+            html_file.write(process_header(mymdLine))
+        elif mymdLine[0:2] == "{{":
+            section = ""
+            level_counter = 0
+            while level_counter > 0:
+                level_counter += len(re.findall("{", mymdLine)) \
+                              - len(re.findall("}", mymdLine))
+                section += mymdLine + "\n"
+                mymdLine = mymd.readline()
+            html_file.write(process_button(section))
+
+
+
+
+
 
 '''
 Close it up!
 '''
-template.close()
-html.close()
-mymd.close()
+template_file.close()
+html_file.close()
+mymd_file.close()
