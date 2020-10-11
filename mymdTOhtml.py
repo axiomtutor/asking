d10260d59a2057bd0823ccf6863425da18ea36a5
axiomtutor <axiomtutor@gmail.com>
2020-10-11 18:25:46 -0400

    mymd

--- mymdTOhtml.py
+++ mymdTOhtml.py
@@ -2,6 +2,28 @@ import sys
 from pathlib import PurePosixPath, Path
 import re
 
+### Description ###
+# This processes a mymd file, which is a .txt file containing my own markup
+# which gets translated to HTML.
+
+### Dexcription of mymd ###
+# A mymd file is meant to represent notes from a chapter of a textbook.
+# It always starts with:
+# Title - Author Name - What Goes in <title> tags.
+# and then
+# * Chapter Number
+# * Chapter Title
+# and then sections contained in zoom buttons, which themselves can contain
+# other zooms which may be subsections, definitions, theorems, exercises, and
+# so on.  Sectioning is done entirely by the {{Buton Title} Button content.}
+# notation.  Sections semantically tagged, for instance if a section is a
+# definition then it can be written
+# {{Definition of a *partition*}#def A *partition* is a ...}
+# and tags can be stacked.
+# {{How do you generalize the ...}#theorem#exercise One way is to ...}
+# Inside of any curly braces, text is processed so that *b* transoforms to
+# bold, **i** transforms to italics.
+
 '''
 Establishing file paths, open files
 '''
@@ -54,6 +76,10 @@ for i in range(51):
 '''
 Process the mymd
 '''
+def process_markup(text):
+    # Turn bold and italics marks to HTML
+    
+
 def process_header(line):
     # Number of stars indicates level of header.
     if line[0] == "*":
@@ -71,22 +97,42 @@ def process_header(line):
         htmlLine += str(n)+">"
         # Now put everything else in the file, and close header.
         end_index_of_stars = re.search('*+').span[1]
-        htmlLine += line[end_index_of_stars+1:]+) + "</h" + str(n) + ">"
+        remaining_text = line[end_index_of_stars+1:]
+        remaining_text = process_markup(remaining_text)
+        htmlLine += remaining_text + "</h" + str(n) + ">"
     else: # If there are no stars, somehting went wrong.
         raise("process_header applied to a line that doesn't start with *")
 
-def process_button(section):
+def process_button(section, context):
     # A section is a string of all the text inside a button environment.
-    return "process_botton TODO\n"
+    # The context is a list indicating the location, in the sense of a tree
+    # with children and siblings, etc.  It is mostly for an id naming scheme.
+    out = '<div class="zoomInterface">\n<button aria-expanded="false" class="zoomButton" id="' + ".".join(context) +
+
+    curly_1_index = re.search("{", section).span[1]
+    curly_2_index = re.search("{", section[curly_1_index+1:]).span[1]
+
+    for line in section.split("\n"):
+        if "}" in line:
+
+    return "process_button TODO\n"
 
 mymdLine = mymd_file.readline()
+instance_counter = 0
+chapter_counter = 0
+new_chapter = true
 while mymdLine:
     if (len(mymdLine) == 0 || mymdLine.isspace()):
         html_file.write("<br>")
     else:
         if mymdLine[0] == "*":
+            if new_chapter:
+                chapter += 1
+                new_chapter = false
             html_file.write(process_header(mymdLine))
         elif mymdLine[0:2] == "{{":
+            new_chapter = true
+            instance_counter += 1
             section = ""
             level_counter = 0
             while level_counter > 0:
@@ -94,7 +140,8 @@ while mymdLine:
                               - len(re.findall("}", mymdLine))
                 section += mymdLine + "\n"
                 mymdLine = mymd.readline()
-            html_file.write(process_button(section))
+            html_file.write(process_button(section, [chapter_counter,instance_counter]))
+    mymdLine = mymd.readline()
 
 
 
